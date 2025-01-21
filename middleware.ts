12d2ab548edebd18f39d9c156638a9aa24dfa8d0
Alvaro R Martin <83295669+exagonsoft@users.noreply.github.com>
2025-01-21 04:17:35 +0100

    #56 Update Cors

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,8 @@ export function middleware(req: NextRequest) {
   const allowedOrigins = [
     "http://localhost:3000", // Local development
     "https://yourdomain.com",
-    "https://master.d2bdppuk7eu8fy.amplifyapp.com"
+    "https://master.d2bdppuk7eu8fy.amplifyapp.com",
+    "https://subastas.client.martinnotaryfl.com"
   ];
 
   if (origin && !allowedOrigins.includes(origin)) {
